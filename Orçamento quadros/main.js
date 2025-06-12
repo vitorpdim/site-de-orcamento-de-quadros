86ad01eda7c9b0f896ed60faf93787be3feedde0
vitorpdim <vitor.cynh@gmail.com>
2025-06-12 05:52:14 -0300

    main.js

--- Orçamento quadros/main.js
+++ Orçamento quadros/main.js
@@ -1,25 +1,21 @@
-// main.js (VERSÃO FINAL E MAIS COMPATÍVEL)
-
 const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const url = require('url');
 
-let mainWindow; // Torna a janela principal uma variável global
+let mainWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 800,
     webPreferences: {
-      // Estas configurações são essenciais para aplicações locais
       nodeIntegration: true,
       contextIsolation: false,
-      webSecurity: false, // Necessário para carregar arquivos locais
+      webSecurity: false,
     },
-    icon: path.join(__dirname, 'icon.png') // Opcional, se você tiver um ícone
+    icon: path.join(__dirname, 'icon.png')
   });
 
-  // Constrói o caminho para o index.html
   const startUrl = url.format({
     pathname: path.join(__dirname, 'site-orcamento/index.html'),
     protocol: 'file:',
@@ -27,8 +23,6 @@ function createWindow () {
   });
 
   mainWindow.loadURL(startUrl);
-
-  // Abre o DevTools apenas se não estiver em produção (instalado)
   if (!app.isPackaged) {
     mainWindow.webContents.openDevTools();
   }
@@ -38,14 +32,10 @@ function createWindow () {
   });
 }
 
-// Tenta iniciar o servidor backend
 try {
   require('./backend_orcamento/server.js');
 } catch (error) {
-  // Este dialog só funcionará se o app já estiver 'ready'
-  // O ideal é logar e mostrar o erro na janela, se possível
   console.error('Erro fatal no backend ao iniciar:', error);
-  // Vamos mostrar o erro depois que o app estiver pronto
   app.on('ready', () => {
     dialog.showErrorBox(
       'Erro Fatal no Backend',
